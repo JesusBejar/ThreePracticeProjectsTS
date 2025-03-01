15694b5c801637b0e22aa8058b0929b624700b9e
Jesus Bejar <israelbejarii@gmail.com>
2025-03-01 11:16:00 -0700

    made initial comments

--- Task-Tracker/src/main.ts
+++ Task-Tracker/src/main.ts
@@ -1,24 +1,11 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+// get elements
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+// event listener to add event to local list
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+// create list item function
+
+// event listener to delete from local list
+
+// event listener to add event to API 
+
+// event listener to delete from API
