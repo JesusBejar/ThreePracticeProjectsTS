bc2bac5c38b4ff154b3a5381b3bb00bcb5aa89bc
Jesus Bejar <israelbejarii@gmail.com>
2025-03-01 14:52:38 -0700

    made second event listener

--- Task-Tracker/src/main.ts
+++ Task-Tracker/src/main.ts
@@ -6,7 +6,7 @@ const saveBtn = document.querySelector("#sync-btn")
 
 console.log('zero')
 // event listener to add event to local list
-addBtn.addEventListener('click', (e: MouseEvent) => {
+addBtn?.addEventListener('click', (e: MouseEvent) => {
     console.log('one')
     e.preventDefault()
     createItem(input.value)
@@ -19,13 +19,24 @@ function createItem(input: string) {
         <div class="task" data-id="temp-${Date.now()}">
       <input type="checkbox" class="task-check">
       <span class="task-title">${input}</span>
-      <button class="delete-btn">Delete</button>
+      <button class="delete-btn" id="delete-btn">Delete</button>
     </div>`;
-    // what is beforeend??
+    // beforeend means inside the HTML, afterbegin moves most recently made task to the top
     localList.insertAdjacentHTML("beforeend", item)
 }
+console.log('four')
 
 // event listener to delete from local list
+localList?.addEventListener('click', (event: MouseEvent) => {
+    const target = event.target as HTMLElement
+    // contains() === include()??
+    if (target.classList.contains("delete-btn")) {
+        console.log('five')
+        const taskElement = target.closest(".task") as HTMLDivElement
+        taskElement.remove()
+    }
+
+})
 
 // event listener to add event to API 
 
