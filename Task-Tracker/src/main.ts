c781dcf70cae146f47ee6693cb936e9fc3ca8105
Jesus Bejar <israelbejarii@gmail.com>
2025-03-01 14:18:35 -0700

    made event listener and createItem func

--- Task-Tracker/src/main.ts
+++ Task-Tracker/src/main.ts
@@ -1,12 +1,24 @@
 // get elements
-const input = document.querySelector("#task-title")
-const addBtn = document.querySelector("#add-task")
-const localList = document.querySelector("#task-list")
-const saveBtn = document.querySelector("#sync-btn")
+const input = document.querySelector("#task-title") as HTMLInputElement
+const addBtn = document.querySelector("#add-task") as HTMLButtonElement;
+const localList = document.querySelector("#task-list") as HTMLDivElement
+// const saveBtn = document.querySelector("#sync-btn")
 
+console.log('zero')
 // event listener to add event to local list
+addBtn.addEventListener('click', (e: MouseEvent) => {
+    console.log('one')
+    e.preventDefault()
+    createItem(input.value)
+});
 
 // create list item function
+function createItem(input: string) {
+    console.log('two')
+    const item = document.createElement('p')
+    item.innerHTML = input
+    localList.append(item)
+}
 
 // event listener to delete from local list
 
